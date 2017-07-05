a6a3a6ed0d9e7d822b5b27132a2cb43e6a3670db
brm_perezd <alexander.perez@brm.com.co>
2017-07-05 17:34:17 -0500

    Nueva subida

--- crearchivo.php
+++ crearchivo.php
@@ -5,7 +5,7 @@
 
 	//$_FILES["fileToUpload"]["tmp_name"];
 
-
+	/*
 	if(isset($_FILES['miarchivo']['name']) && $_POST['ruta']){
 
 		$imgOriginal=$_FILES['miarchivo']['name'];
@@ -16,17 +16,6 @@
 			$temporal = $_FILES["miarchivo"]["tmp_name"];
 			$urlDef = $rutaf."/";
 			$guarda = move_uploaded_file($temporal, $urlDef.$imgOriginal);//$guarda true si guardo la factura en la carpeta recien creada
-
-			if(file_exists($temporal))
-			{
-			   echo "El archivo ha sido cargadao al servidor";
-			}
-			else
-			{
-			   echo "El archivo no se ha cargado al servidor";
-
-			}
-
 
 
 				if ($guarda) { //si guarda la imagen en la carpeta
@@ -40,3 +29,42 @@
 
 		}	
 	}	
+
+	*/
+
+	
+
+	if(isset($_FILES['miarchivo']['name']) && $_POST['ruta']){
+
+		$imgOriginal=$_FILES['miarchivo']['name'];
+		$rutaf = $_POST['ruta'];
+
+		define("UPLOADEDFILES", $rutaf);
+
+		if($imgOriginal){
+
+			$temporal = $_FILES["miarchivo"]["tmp_name"];
+			$newName = UPLOADEDFILES . $_FILES['ufile']['name']; 
+
+			$guarda = move_uploaded_file($temporal, $newName);//$guarda true si guardo la factura en la carpeta recien creada
+
+
+				if ($guarda) { //si guarda la imagen en la carpeta
+					
+					echo "Archivo subido";
+
+				}else{// si no gurado la imagen en la carpeta
+					echo "Paila candonga Arete, no subio nada";
+
+				}
+
+		}	
+	}	
+
+
+
+
+
+
+
+
