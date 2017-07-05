46202451e44a36ebaf1b8db1cb5aea08828fd8fb
brm_perezd <alexander.perez@brm.com.co>
2017-07-05 11:00:46 -0500

    Se agregan los archivos para probar la creacion de carpeta

---
+++ creacarpeta.php
@@ -0,0 +1,55 @@
+<?php
+
+	if(isset($_POST['nomca'])){
+		$nombreca =$_POST['nomca'];
+		recibenombre($nombreca);
+	}
+
+	
+	//$dir=$this->crearCarpeta($parametros[0]); 
+
+	function recibenombre($nombreca){
+
+		$destino=$_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['REQUEST_URI'])."/".$nombreca;
+		$directorio=array();
+		//var_dump($destino);
+		//echo getcwd();
+		$dir = mkdir($destino,'0777');
+
+		if($dir){
+			echo "Carpeta creada";
+		}
+
+		$directorio[0]=$dir;
+		$directorio[1]= $destino;
+		//return $directorio;
+
+		?>
+			<!DOCTYPE HTML>
+			    <HEAD>
+			        <TITLE>Crear carpeta</TITLE>
+			    </HEAD>
+
+			    <BODY>
+			    	<form action="crearchivo.php" method="post" enctype="multipart/form-data">
+				    	<input type="file" name="miarchivo">
+				    	<input type="hidden" name="ruta" value="<?php echo $directorio[1];  ?>">
+				    	<input type="submit" value="Subir archivo a carpeta">
+				    </form>	
+			    </BODY>
+			</HTML>
+
+		<?php
+
+	}	
+
+
+?>
+
+
+
+
+
+	
+
+	
---
+++ crearchivo.php
@@ -0,0 +1,31 @@
+<?php
+
+
+//var_dump($parametros[10]);
+
+	//$_FILES["fileToUpload"]["tmp_name"];
+
+
+	if(isset($_FILES['miarchivo']['name']) && $_POST['ruta']){
+
+		$imgOriginal=$_FILES['miarchivo']['name'];
+		$rutaf = $_POST['ruta'];
+
+		if($imgOriginal){
+
+			$temporal = $_FILES["miarchivo"]["tmp_name"];
+			$urlDef = $rutaf."/";
+			$guarda = move_uploaded_file($temporal, $urlDef.$imgOriginal);//$guarda true si guardo la factura en la carpeta recien creada
+
+
+				if ($guarda) { //si guarda la imagen en la carpeta
+					
+					echo "Archivo subido";
+
+				}else{// si no gurado la imagen en la carpeta
+					echo "Paila candonga Arete, no subio nada";
+
+				}
+
+		}	
+	}	
